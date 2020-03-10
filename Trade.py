93e9842c5076348d3a101a2828e18b0fa5690df5
Amit Kumar <charayaamit@gmail.com>
2020-03-10 19:14:43 +0530

    first commit

---
+++ Trade.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datetime import date
+from nsepy import get_history
+from pandas_datareader import data as pdr
+import yfinance as yf
+import matplotlib.pyplot as plt
+
+sbin=get_history("SBIN",start=date(2020,1,1),end=date(2020,3,10))
+sbin_close=sbin[['Close']]
+#sbin_close.set_index('Date',inplace=True)
+print((sbin_close))
+plt.figure(figsize=(14,5))
+
+plt.plot(sbin_close,'b')
+plt.plot(sbin_close,'ro')
+plt.grid(True)
+plt.title("SBIN Close price")
+plt.xlabel("Date")
+plt.ylabel("Price")
+plt.plot(sbin_close)
+plt.show()
+
+
